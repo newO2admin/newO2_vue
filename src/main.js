8e0bc27321efe5e7c179004645866d5ef55ae1f2
czc <947973799@qq.com>
2019-11-22 09:18:22 +0800

    静态页面

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'lib-flexible/flexible'
+
+import router from './router'
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+new Vue ({
+  el: '#app',
+  components: {App},
+  template: '<App />',
+  //安装路由
+  router
+})
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
