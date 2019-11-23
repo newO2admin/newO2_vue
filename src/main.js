8fd5aa659cdcd8746b56ef2424441068fe0c3b22
gossipphoto <577929827@qq.com>
2019-11-23 11:23:42 +0800

    1

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import 'lib-flexible/flexible'
 
 import router from './router'
+import store from './store'
 // import './veevalidate'
 
 Vue.config.productionTip = false
@@ -13,6 +14,7 @@ new Vue ({
   template: '<App />',
   //安装路由
   router,
+  store
 })
 // new Vue({
 //   render: h => h(App),
