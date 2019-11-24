491de8e58789a301d63908145897e1d8287aef6e
czc <947973799@qq.com>
2019-11-24 14:12:12 +0800

    cc

--- src/store/actions.js
+++ src/store/actions.js
@@ -4,7 +4,8 @@ import {SAVE_FOOTITEM} from './mutation-type'
 export default{
   async getFootItemAction({commit}){
     //1. 发送请求获取数据
-    let result = await getFootItem
+    let result = await getFootItem()
+    console.log(result)
     if(result.code === 0){
       // 2. 调用mutation，将数据交给mutation
       commit(SAVE_FOOTITEM, {footItems: result.data})
