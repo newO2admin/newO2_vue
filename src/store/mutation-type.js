767e4831db23a15e432428d1dd770bf566a46994
gossipphoto <577929827@qq.com>
2019-11-25 10:17:43 +0800

    1

--- src/store/mutation-type.js
+++ src/store/mutation-type.js
@@ -2,3 +2,9 @@
 export const SAVE_BEAUTIFUL = 'save_beautiful'
 //czc
 export const SAVE_FOOTITEM = 'save_footitem'
+
+export const SAVE_USER = 'save_user'
+
+export const SAVE_TOKEN = 'save_token'
+
+export const SAVE_CYCLOPEDIA = 'save_cyclopedia'
--- src/store/mutations-type.js
+++ src/store/mutations-type.js
@@ -3,3 +3,8 @@ export const SAVE_USER = 'save_user'
 export const SAVE_TOKEN = 'save_token'
 
 export const SAVE_CYCLOPEDIA = 'save_cyclopedia'
+
+//lmx
+export const SAVE_BEAUTIFUL = 'save_beautiful'
+//czc
+export const SAVE_FOOTITEM = 'save_footitem'
